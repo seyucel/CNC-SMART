91cde89f912805b79c1d5f58f576cdb83d979e5c
Sezen Yucel <31349707+seyucel@users.noreply.github.com>
2022-09-12 11:59:34 -0400

    Update and rename SMART_TEM_1.m to SMART_TEM1.m

--- SMART_TEM1.m
+++ SMART_TEM1.m
@@ -1,9 +1,11 @@
-%% the first parameter to be entered by the user:
-pixel_length= 0.33;
 %% to select and upload the TEM images
+%clear all,close all, clc
 [path,user_cance] = imgetfile('MultiSelect','on');
 nfiles = length(path);
 ims = cell(nfiles,1);
 for i = 1:nfiles
     ims{i} = imread(path{i});
 end
+%% the first input to be entered by the user:
+input1 = inputdlg('Enter the pixel length (nm):');
+pixel_length = str2num(input1{1});
