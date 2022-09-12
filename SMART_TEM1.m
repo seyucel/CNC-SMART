605ad8c277f333261f9821b17983286a02e31356
Sezen Yucel <31349707+seyucel@users.noreply.github.com>
2022-09-12 12:01:02 -0400

    Update SMART_TEM1.m

--- SMART_TEM1.m
+++ SMART_TEM1.m
@@ -6,6 +6,6 @@ ims = cell(nfiles,1);
 for i = 1:nfiles
     ims{i} = imread(path{i});
 end
-%% the first input to be entered by the user:
+%% the first input to be entered by the user
 input1 = inputdlg('Enter the pixel length (nm):');
 pixel_length = str2num(input1{1});
